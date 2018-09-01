e214ec26ca55defc7137c1e4213117193dea4430
fdistorted <andrejjrom@gmail.com>
2018-09-01 17:55:02 +0300

    inital commit, minimum working skill

---
+++ lambda/custom/index.js
@@ -0,0 +1,134 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+const axios = require('axios');
+const utils = require('./skill-utils');
+
+const links = {
+  full: "https://s3.us-east-2.amazonaws.com/mortgage-rates-service/full.json",
+}
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  async handle(handlerInput) {
+    var speechText = 'Welcome to mortgage rates skill. ';
+
+    //comment this on production
+    process.env.RATE_TRESHOLD = 5;
+
+    let result = await axios.get(links.full);
+
+    let mortgages = result.data.mortgages
+
+    speechText += "Mortgage reates are: ";
+    let passedTreshold = 0;
+
+    for (let i = 0; i < mortgages.length; i++) {
+      let lowestRate = utils.getLowestRate(mortgages[i].rates);
+      if (lowestRate < process.env.RATE_TRESHOLD) {
+        passedTreshold++;
+        speechText += `for ${mortgages[i].provider} it is ${lowestRate}; `
+      }
+    }
+
+    if (passedTreshold) {
+      speechText += "Would you like to hear all quotes for a specific provider?' If so, please say: Yes, for and the provider name";
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .getResponse();
+    } else {
+      speechText = "sorry but i cant find Mortgage rates lowest than treshold, please try again later."
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .getResponse();
+    }
+  },
+};
+
+const ReadProviderIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'ReadProviderIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'read provider intent hello';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .getResponse();
+  },
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const speechText = 'You can say hello to me!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent' ||
+        handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}\n${error.stack}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    ReadProviderIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
---
+++ lambda/test/test.js
@@ -0,0 +1,52 @@
+const skill = require('../custom/index');
+const axios = require('../custom/node_modules/axios')
+
+process.env.RATE_TRESHOLD = 5;
+
+
+async function main() {
+
+    const links = {
+        full: "https://s3.us-east-2.amazonaws.com/mortgage-rates-service/full.json",
+    }
+
+    var  speechText = 'Welcome to mortgage rates skill. ';
+
+    let result = await axios.get(links.full);
+
+    let mortgages = result.data.mortgages
+
+    speechText += "Mortgage reates are: ";
+    let passedTreshold = 0;
+
+    for (let i = 0; i < mortgages.length; i++) {
+      let lowestRate = getLowestRate(mortgages[i].rates);
+      if (lowestRate < process.env.RATE_TRESHOLD) {
+        passedTreshold++;
+        speechText += `for ${mortgages[i].provider} it is ${lowestRate}; `
+      }
+    }
+
+    if (passedTreshold) {
+      speechText += "Would you like to hear all quotes for a specific provider?' If so, please say: \"Yes, for\" and the provider name";
+    //   return handlerInput.responseBuilder
+    //     .speak(speechText)
+    //     .reprompt(speechText)
+    //     .getResponse();
+    } else {
+      speechText = "sorry but i cant find Mortgage rates lowest than treshold, please try again later."
+    //   return handlerInput.responseBuilder
+    //     .speak(speechText)
+    //     .getResponse();
+    }
+
+    console.log(speechText)
+
+    skill.handler("test", "", function (result, error) {
+        console.log("done");
+    });
+}
+
+main();
+
+
